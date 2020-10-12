39cf33c4cc7f1dc8aabccc3a85f91fa9307a5994
Cliona Hayden <c00225401@itcarlow.ie>
2020-10-12 11:03:10 +0100

    Sprint 3 - Steps 9 - 12 complete

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -7,14 +7,24 @@ public class TestSuite
 {
     private Game game;
 
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject =
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(game.gameObject);
+    }
+
     // 1
     [UnityTest]
     public IEnumerator AsteroidsMoveDown()
     {
-        // 2
-        GameObject gameGameObject =
-            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
-        game = gameGameObject.GetComponent<Game>();
         // 3
         GameObject asteroid = game.GetSpawner().SpawnAsteroid();
         // 4
@@ -23,8 +33,45 @@ public class TestSuite
         yield return new WaitForSeconds(0.1f);
         // 6
         Assert.Less(asteroid.transform.position.y, initialYPos);
-        // 7
-        Object.Destroy(game.gameObject);
+        
+    }
+
+    [UnityTest]
+    public IEnumerator GameOverOccursOnAsteroidCollision()
+    {
+
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        //1
+        asteroid.transform.position = game.GetShip().transform.position;
+        //2
+        yield return new WaitForSeconds(0.1f);
+
+        //3
+        Assert.True(game.isGameOver);
+
+    }
+
+    [UnityTest]
+    public IEnumerator NewGameRestartsGame()
+    {
+        //1
+        game.isGameOver = true;
+        game.NewGame();
+        //2
+        Assert.False(game.isGameOver);
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator LaserMovesUp()
+    {
+        // 1
+        GameObject laser = game.GetShip().SpawnLaser();
+        // 2
+        float initialYPos = laser.transform.position.y;
+        yield return new WaitForSeconds(0.1f);
+        // 3
+        Assert.Greater(laser.transform.position.y, initialYPos);
     }
 
 }
